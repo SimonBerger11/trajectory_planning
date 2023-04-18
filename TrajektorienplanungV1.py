af276829d73cdc7e2f52bcb0c8ff90dac7ea56fd
eliashaering <eliashaering@web.de>
2023-04-18 15:00:04 +0200

    Update TrajektorienplanungV1.py

--- TrajektorienplanungV1.py
+++ TrajektorienplanungV1.py
@@ -6,10 +6,30 @@ tree = ET.parse('Vorabkarte_mit_Parkplaetze_3.xml')
 root = tree.getroot()
 
 
-points = []
+points = {}
 
-for child in root:
-    print(child.tag, child.attrib)
-    if child.tag == "node":
+for node in root.findall("node"):
+    #print(node.tag, node.attrib)
+    point = {}
+    point["x"] = node.attrib["lat"]
+    point["y"] = node.attrib["lon"]
+    #points["Typ"] = ""
+    points[node.attrib["id"]] = point
+
+for way in root.findall("way"):
+    type = ""
+    for tag in way.findall("tag"):
+        
+        if tag.attrib["k"] == "type":
+            type = tag.attrib["v"]
+    if type == "parking_space":
+        for nd in way.findall("nd"):
+            #print(nd.attrib["ref"])
+            points[nd.attrib["ref"]]["Typ"] = type
+
+
+for point in points:
+    print(point, points[point])
+
 
 
