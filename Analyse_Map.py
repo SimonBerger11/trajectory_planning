ed1c9028646c42c6c1e9facacda3bab9e7ffa590
eliashaering <eliashaering@web.de>
2023-05-03 09:11:18 +0200

    update

--- Analyse_Map.py
+++ Analyse_Map.py
@@ -8,10 +8,10 @@ import numpy as np
 def way_hor(y, x_offset,length,res, invers):
     w_ruck = []
     if invers == False:
-        for i in range(0,int(length* (1/res))):
+        for i in range(0,int((length +1)* (1/res))):
             w_ruck.append((x_offset+ i*res,y,0))
     else:
-        for i in range(int(length* (1/res)),0,-1):
+        for i in range(int(length* (1/res)),-1,-1):
             w_ruck.append((x_offset+ i*res,y,0))
     return w_ruck
 
@@ -19,10 +19,10 @@ def way_hor(y, x_offset,length,res, invers):
 def way_ver(x,y_offset,length,res, invers):
     w_ruck = []
     if invers == False:
-        for i in range(0,int(length* (1/res))):
+        for i in range(0,int((length +1)* (1/res))):
             w_ruck.append((x,y_offset+(i)*res,0))
     else:
-        for i in range(int(length* (1/res)),0,-1):
+        for i in range(int(length* (1/res)),-1,-1):
             w_ruck.append((x,y_offset+(i)*res,0))
     return w_ruck
 
@@ -34,11 +34,11 @@ def createLines(res):
     way.append(way_hor(14, 5, 10,res,False))
 
     way.append(way_ver(14, 5, 9,res,False))
-    #way.append(way_ver(12, 5, 9,res,False))
+    way.append(way_ver(12, 5, 9,res,False))
     way.append(way_ver(9, 5, 9,res,False))
     way.append(way_ver(5, 5, 9,res,False))
 
-    way.append(way_hor(5, 5, 10,res,True))
+    #way.append(way_hor(5, 5, 10,res,True))
     way.append(way_hor(9, 5, 10,res,True))
     way.append(way_hor(14, 5, 10,res,True))
 
---
+++ csv_reader.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('eggs.csv', newline='') as csvfile:
+
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    for row in spamreader:
+
+        print(', '.join(row))
