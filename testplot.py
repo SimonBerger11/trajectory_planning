eb9dd62ec2a3be9148b50fc3f4162077388174a7
eliashaering <eliashaering@web.de>
2023-05-09 16:31:19 +0200

    Update testplot.py

--- testplot.py
+++ testplot.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 10, 0, 1])
 
-for i in range(10):
-    y = np.random.random()
-    plt.scatter(i, y)
-    plt.pause(0.05)
+plt.scatter([1,1,2],[1,2,1], color= "green")
+
+
 
 plt.show()
